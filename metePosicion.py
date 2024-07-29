2bea0f10101f4692bee596bc2c643307cf92a091
fer-liftisto <>
2024-07-29 05:04:28 +0200

    incorporo los ficheros tablero.py y metePosicion

---
+++ metePosicion.py
@@ -0,0 +1,64 @@
+'''
+https: // enriquelazcorreta.gitbooks.io/tkinter/content/creando-la-interfaz-grafica-de-usuario-gui/texto-index-tag-y-mark.html
+'''
+#from configuracion import *
+import tkinter.ttk as ttk
+from configuracionTablero import *
+
+
+
+###################################################################
+def posicion(fen_posicion):
+    
+    #FEN = 'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w - 0 1'
+    partida= fen_posicion.get()
+    
+    
+    print(partida)
+    def devuelve_posicion(partida):
+            nombre, torne, FEN, confi = partida.split(',')
+            filas = FEN.split('/')
+            return filas
+        ############################
+    fila = devuelve_posicion(partida)
+        #####################################################################
+
+    def devuelve_fila(fila):
+        tabli = []
+        for f in fila:
+            fil = ' '*8
+            fil = list(fil)
+            con = -1
+            for elemento in f:
+                if elemento.isnumeric():
+                     con += int(elemento)
+                else:
+                    con += 1
+                    fil[con] = elemento
+            tabli.append(fil)
+        return tabli
+
+    ##############################
+    tabli = devuelve_fila(fila)
+    ###########################################################################
+    return tabli
+
+
+def traduce(f):
+    # '♟♜♞♝♛♚' #
+    f = f.replace('R', '♜')
+    f = f.replace('N', '♞')
+    f = f.replace('B', '♝')
+    f = f.replace('Q', '♛')
+    f = f.replace('K', '♚')
+    f = f.replace('P', '♟')
+
+    f = f.replace('r', '♜')
+    f = f.replace('n', '♞')
+    f = f.replace('b', '♝')
+    f = f.replace('q', '♛')
+    f = f.replace('k', '♚')
+    f = f.replace('p', '♟')
+    return f
+
+
